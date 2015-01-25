298e548275fbb3632d690939f8965635a576d34f
Liyang9898 <wingman989898@gmail.com>
2015-01-25 23:14:42 +0800

    Create Move.java

---
+++ Move.java
@@ -0,0 +1,10 @@
+package datastructure;
+
+public class Move {
+	public int x1;
+	public int x2;
+	public int y1;
+	public int y2;
+	public int type;//1=approach 2=withdraw 3=both 4=paika
+	public int utility;//for alpha-beta pruning
+}
